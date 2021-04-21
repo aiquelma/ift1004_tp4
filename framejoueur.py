b9434344594612fdd73ad11912bcfc059584a831
maiquel <aiquel@me.com>
2021-04-21 16:02:35 -0400

    tests

--- framejoueur.py
+++ framejoueur.py
@@ -1,16 +1,15 @@
 from tkinter import Frame, Label, Button
 from pymafia.joueur import Joueur
 
-
 class FrameJoueur(Frame):
-    def __init__(self, master):
+    def __init__(self, master, joueur):
         super().__init__(master)
-        self.label_joueur = Label(self, text=f"joueur{Joueur.identifiant}")
-        self.label_des = Label(self, text=Joueur.des, font=("courrier", 32))
+        self.label_joueur = Label(self, text=f"joueur{joueur.identifiant}")
+        self.label_des = Label(self, text=joueur.dés, font=("courrier", 32))
         self.button_des = Button(self, text="rouler les des")
-        self.button_des.bind("<ButtonRelease-1>", lambda event: self.mettre_a_jour_des(Joueur))
+        self.button_des.bind("<ButtonRelease-1>", lambda event: self.mettre_a_jour_des(joueur))
 
-    def metter_a_jour_des(self, joueur: Joueur):
+    def metter_a_jour_des(self, joueur):
         joueur.rouler_dés()
         self.label_des['text'] = joueur.dés
 
@@ -21,34 +20,30 @@ class FrameJoueur(Frame):
 class FrameJoueurGaucheHaut(FrameJoueur):
     def __init__(self, master, joueur):
         super().__init__(master, joueur)
-        self.label_des['wrapLength'] = 1
         self.label_joueur.grid(row=0, column=0)
         self.label_des.grid(row=0, column=1)
         self.label_des['wrapLength'] = 1
         self.button_des.grid(row=0, column=2)
-
-        button_des = Button(self, text="rouler les dés")
+        # self.label_des['wrapLength'] = 1
+        # button_des = Button(self, text="rouler les dés")
 
 
 class FrameJoueurDroitHaut(FrameJoueur):
     def __init__(self, master, joueur):
         super().__init__(master, joueur)
-        self.label_des['wrapLength'] = 1
-        self.label_joueur.grid(row=0, column=0)
+        self.label_joueur.grid(row=0, column=20)
         self.label_des.grid(row=0, column=1)
-        self.label_des['wrapLength'] = 1
-        self.button_des.grid(row=0, column=2)
-
-        button_des = Button(self, text="rouler les dés")
+        self.label_des['wraplength'] = 1
+        self.button_des.grid(row=0, column=0)
 
 
 class FrameJoueurGaucheBas(FrameJoueur):
     def __init__(self, master, joueur):
         super().__init__(master, joueur)
-        self.label_des['wrapLength'] = 1
+        self.label_des['wraplength'] = 1
         self.label_joueur.grid(row=0, column=0)
         self.label_des.grid(row=0, column=1)
-        self.label_des['wrapLength'] = 1
+        self.label_des['wraplength'] = 1
         self.button_des.grid(row=0, column=2)
         boutonJ1 = Button(text='rouler les dés')
         boutonJ1.grid(row=2, column=2)
@@ -60,10 +55,10 @@ class FrameJoueurGaucheBas(FrameJoueur):
 class FrameJoueurDroitBas(FrameJoueur):
     def __init__(self, master, joueur):
         super().__init__(master, joueur)
-        self.label_des['wrapLength'] = 1
+        self.label_des['wraplength'] = 1
         self.label_joueur.grid(row=0, column=0)
         self.label_des.grid(row=0, column=1)
-        self.label_des['wrapLength'] = 1
+        self.label_des['wraplength'] = 1
         self.button_des.grid(row=0, column=2)
 
         button_des = Button(self, text="rouler les dés")
